d0a04932f9cc46cf3dc9967fdbca824384883d9c
ql <pengj@hrcytech.com>
2021-04-20 17:33:29 +0800

    add some data

---
+++ core/src/com/qlang/game/demo/screen/GameCreateScreen.kt
@@ -0,0 +1,17 @@
+package com.qlang.game.demo.screen
+
+import com.badlogic.gdx.ScreenAdapter
+
+class GameCreateScreen : ScreenAdapter() {
+    init {
+
+    }
+
+    override fun render(delta: Float) {
+
+    }
+
+    override fun dispose() {
+
+    }
+}
---
+++ core/src/com/qlang/game/demo/screen/WorlListScreen.kt
@@ -0,0 +1,7 @@
+package com.qlang.game.demo.screen
+
+import com.badlogic.gdx.ScreenAdapter
+
+class WorlListScreen : ScreenAdapter() {
+
+}
